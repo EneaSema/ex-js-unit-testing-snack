9e07375d7838a9ded069bfa0d171383685de1c6f
EneaSema <sema.enea1993@gmail.com>
2025-10-21 23:18:16 +0200

    resolve forever snack 1 guardando video e confrontandomi con Gemini

--- src/assets/js/snacks.js
+++ src/assets/js/snacks.js
@@ -1,12 +1,19 @@
 // 🏆 Snack 1
 // Creare un test che verifichi la seguente descrizione:
-
 // 👉 "La funzione getInitials restituisce le iniziali di un nome completo."
 
 function getInitials(nomeCompleto) {
-  const [nome, cognome] = nomeCompleto.split(" ").filter((str) => str !== " ");
+  const [nome, cognome] = nomeCompleto.split(" ").filter((str) => str !== ``);
 
   return `${nome.charAt(0).toUpperCase()}.${cognome.charAt(0).toUpperCase()}.`;
 }
 
-module.exports = { getInitials };
+// 🏆 Snack 2
+// Creare un test che verifichi la seguente descrizione:
+// 👉 "La funzione createSlug restituisce una stringa in lowercase."
+
+function createSlug(stringa) {
+  return stringa.toLowerCase();
+}
+
+module.exports = { getInitials, createSlug };
--- src/tests/snacks.test.js
+++ src/tests/snacks.test.js
@@ -1,12 +1,19 @@
-const { getInitials } = require("../assets/js/snacks.js");
+const { getInitials, createSlug } = require("../assets/js/snacks.js");
 
 // 🏆 Snack 1
 // Creare un test che verifichi la seguente descrizione:
-
 // 👉 "La funzione getInitials restituisce le iniziali di un nome completo."
 
 test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
   expect(getInitials("Enea Sema")).toBe("E.S.");
   expect(getInitials("aurora sema")).toBe("A.S.");
-  expect(getInitials("Lupo  nero")).toBe("L.N.");
+  expect(getInitials("Lupo  Nero")).toBe("L.N.");
+});
+
+// 🏆 Snack 2
+// Creare un test che verifichi la seguente descrizione:
+// 👉 "La funzione createSlug restituisce una stringa in lowercase."
+
+test("La funzione createSlug restituisce una stringa in lowercase.", () => {
+  expect(createSlug("ENEA")).toBe("enea");
 });
